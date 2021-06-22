e9b2a495d4a7c1544494db002795ef25d0ec8053
amartelai <55350885+amartelai@users.noreply.github.com>
2021-06-22 16:11:11 +0200

    Finished with small correection

--- app.js
+++ app.js
@@ -1,98 +1,79 @@
-fetchPosts()
-function fetchPosts(){
+fetchData();
+function fetchData() {
     fetch("data.json")
-        .then((response) =>{
-            return response.json()
+        .then(response => {
+            return response.json();
         })
-        .then(data =>{
-            console.log(data)
-            const userData = data.map((item) =>{
-                let socialPhoto='';
-                if (item.source_type === "facebook")
-                     socialPhoto = "./icons/facebook.svg"
+        .then(data => {
+            const html = data
+                .map(post => {
+                    let postlogo = "";
+                    if (post.source_type==="facebook")
+                        postlogo = "icons/facebook.svg";
+                    else
+                        postlogo = "icons/instagram-logo.svg";
 
-                else
-                    socialPhoto ="./icons/instagram-logo.svg"
+                    const date = new Date(post.date);
+                    let formattedDate = date.toLocaleDateString('en-GB', {
+                        day: 'numeric', month: 'short', year: 'numeric'
+                    })
 
-                const date = new Date(item.date);
-                let formated_date = date.toLocaleDateString('en-GB', {
-                    day: 'numeric', month: 'short', year: 'numeric'
-                })
-                return `
-                        <div class = "post" name = "post" style="display: none" >
-                            <div class = "profile-header">
-                                <div class = "profile-image" >
-                                     <img src = "${item.profile_image}"  /> 
+                    return `<div class="post" name="post" style="display: none" onclick="showPost(this)">
+                                <div class="profile">
+                                    <div class="profile-image"><img src="${post.profile_image}"></div> 
+                                    <div class="profile-info"><div class="name">${post.name}</div><div class="date">${formattedDate}</div></div>
+                                    <div class="logo"><img src="${postlogo}"></div>
                                 </div>
-                                 <div class = "username">
-                                   ${item.name} 
-                                 </div>
-                                 <div class = "soc_media">
-                                        <img class = "social-media" src="${socialPhoto}" /> 
-                                 </div>
-                                
+                                <div class="image"><img src="${post.image}"></div>
+                                <div class="caption">${post.caption}</div>
+                                <hr>
+                                <div class="likes"><img src="icons/heart.svg"> 
+                                        <div style = "    margin-left: 20px; margin-top: -18px; margin-left: 23px;">
+                                                ${post.likes}
+                                        </div>
+                                </div>
                             </div>
-                             <div class = "date">
-                                ${formated_date}
-                             </div>
-                             <div class = "photo"  >
-                                 <img src = "${item.image}" style = "width: 100%;
-                                     height: 350px; object-fit: cover; margin-top: 15px; " /> 
-                            </div> 
-                            <div class = "description">
-                                ${item.caption}
-                            </div>
-                            <div>
-                            <hr style = "border: 0.1px solid #e4e3e0">
-                            </div>
-                            <div class="favorite-icon">
-                                  <img src="./icons/heart.svg" style="height: 20px; width: 20px; float: left; margin-right: 6px " />    
-                            </div>  
-                            <div class = "favorite">
-                                ${item.likes}
-                            </div>   
-                        </div>
-                `
-            }).join("")
-            document.querySelector(".posts").insertAdjacentHTML("beforeend", userData)
+                            `;
+                }).join("");
+
+
+            document.querySelector(".posts").insertAdjacentHTML("beforeend", html);
+
         })
         .then(loadMore)
 }
-
-
-    //
-    // const loadMore = document.querySelector("#btn")
-    // let currentItems = 2;
-    // loadMore.addEventListener('click', (e) => {
-    //     const postsList = [...document.querySelectorAll('.posts .post')];
-    //     console.log(postsList)
-    //     console.log("HSd")
-    //     for(let i = currentItems; i < currentItems + 2; i++)
-    //         if(postsList[i])
-    //             postsList[i].style.display = "block"
-    //
-    //     currentItems += 2;
-    //     if(currentItems >= postsList.length)
-    //         e.target.style.display="none"
-    //
-    // })
-
-function loadMore() {
-    let br = 0;
+function loadMore(){
+    let br=0;
     let posts = document.getElementsByName("post");
-    let buton = document.getElementById("btn");
-    for (let i = 0; i < posts.length; i++) {
+    let buton = document.getElementById("buton");
+    for (let i=0;i<posts.length;i++){
         if (posts[i].style.display === "none") {
             posts[i].style.display = "block";
             br++;
         }
-        if (posts[i + 1] === undefined)
+        if (posts[i+1]===undefined)
             buton.style.display = "none";
-        if (br === 4)
+        if (br===4)
             return;
     }
 }
 
+function showPost(post){
 
+    post.removeAttribute("onclick");
+    let profile = post.childNodes[1];
+    let image = post.childNodes[3];
+    let caption = post.childNodes[5];
+    let likes = post.childNodes[9];
 
+    post = `<div class="post">${image.outerHTML}${profile.outerHTML}<hr>${caption.outerHTML}${likes.outerHTML}</div>`;
+    post = new DOMParser().parseFromString(post, "text/html").body.firstChild;
 
+    console.log(post);
+    post.style.border = "none";
+    post.style.padding = "0";
+    let modal = document.getElementById("myModal");
+
+    document.querySelector(".modal-content").append(post);
+    modal.style.display = "block";
+}
